de75e4f98a974e15776d64938771fd792dd21a00
hamed ebrahimi <hamedebrahimi128@gmail.com>
2025-03-17 17:19:38 +0330

    Create background text

---
+++ src/components/backgroundText/index.tsx
@@ -0,0 +1,14 @@
+const BackgroundText = () => {
+  return (
+    <>
+      <span className="absolute top-2 text-purple-secondary/50 text-[256px] font-bold">
+        Wednesday
+      </span>
+      <span className="absolute bottom-2  text-purple-secondary/50 text-[316px] font-bold">
+        Midnight
+      </span>
+    </>
+  );
+};
+
+export default BackgroundText;
